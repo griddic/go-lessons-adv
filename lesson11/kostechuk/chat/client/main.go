cc5882d8909c00f20b02539b312f2d24f7ce58c4
Mikhail K <mkostechuk@gmail.com>
2017-09-18 23:51:38 +0300

    Client read/write first rough implementation

--- lesson11/kostechuk/chat/client/main.go
+++ lesson11/kostechuk/chat/client/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
-	"time"
 )
 
 func main() {
 	conn, err := net.Dial("tcp", "127.0.0.1:2222")
 	if err != nil {
-		log.Fatal("Cannot connect", err)
+		log.Fatal("Cannot connect ", err)
 	}
 	defer conn.Close()
-	conn.Write([]byte("Hello!"))
-	time.Sleep(time.Minute * 60)
+
+	go func() {
+		rd := bufio.NewReader(conn)
+		for {
+			str, err := rd.ReadString('\n')
+			if err != nil {
+				log.Fatal("Connection lost ", err)
+			}
+			fmt.Println(str)
+		}
+	}()
+
+	for {
+		input := ""
+		_, err = fmt.Scanln(&input)
+		input = input + "\n"
+		conn.Write([]byte(input))
+	}
+	// time.Sleep(time.Second * 30)
 }
